692ed397a1191eaa7e7d9689ec07e8f847354816
martucazpo <martucazpo@gmail.com>
2020-04-06 10:30:01 -0700

    routes are connected

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,81 @@
 
 const router = require('express').Router();
-//const User = require('../models/users');
+const db = require('../models');
+const User = db.User;
 
 
-router.get('/', (req, res) => {
-    res.send('Hello World');
+router.get('/', async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users)
+    } 
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
+
+// get one user
+
+router.get('/:id', getUser, (req, res) => {
+    res.json(res.user);
+});
+
+// create a user
+
+router.post('/', async (req, res) => {
+    const user = new User({
+        name: req.body.name
+    });
+    try {
+        const newUser = await user.save();
+        res.status(201).json(newUser);
+    } 
+    catch (err){
+        res.status(402).json({ message: err.message });
+    }
+});
+
+// update a user
+
+router.patch('/:id', getUser, async (req, res) => {
+    if (req.body.name !== null){
+        res.user.name = req.body.name;
+    }
+    try {
+        const updatedUser = await res.user.save();
+        res.json(updatedUser);
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// delete a user
+
+router.delete('/:id', getUser, async (req, res) => {
+    try {
+        await res.user.remove();
+        res.json({ message: 'User removed'});
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+async function getUser(req, res, next) {
+    let user;
+    try {
+        user = await User.findById(req.params.id);
+        if (user === null) {
+            return res.status(404).json({ message: "Cannot find user"});
+        }
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+
+    res.user = user;
+    next();
+}
 
 module.exports = router;
